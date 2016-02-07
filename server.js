060539ceb19a0b2179ea14180c6acc6fac0e6eb9
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-02-07 16:08:55 +0200

    Connect with mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config';
+import {MongoClient} from 'mongodb';
 
 let app = express();
 let router = express.Router();
@@ -20,4 +21,17 @@ app.use(webpackHotMiddleware(compiler, {
   log: console.log
 }));
 
-app.listen(3000, console.log("http://localhost:3000/"));
+let db;
+
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+  if (err) throw err;
+  db = database;
+  app.listen(3000, console.log("http://localhost:3000/"));
+});
+
+app.get("/data/links", (req, res) => {
+  db.collection("links").find({}).toArray((err, links) => {
+    if (err) throw err;
+    res.json(links);
+  });
+});
