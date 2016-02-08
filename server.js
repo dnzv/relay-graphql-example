9ebbc10014c16b8d041b01db4e211259221b044a
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-02-08 20:21:28 +0200

    Add GraphQL schema and expose it from the server

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config';
 import {MongoClient} from 'mongodb';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 
 let app = express();
-let router = express.Router();
+//let router = express.Router();
 let compiler = webpack(config);
-
-app.use(express.static('public'));
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
@@ -21,8 +21,16 @@ app.use(webpackHotMiddleware(compiler, {
   log: console.log
 }));
 
+app.use(express.static('public'));
+
+app.use('/graphql', GraphQLHTTP({
+  schema,
+  graphiql: true
+}));
+
 let db;
 
+// export MONGO_URL=mongodb://rgrjs:1234@ds059115.mongolab.com:59115/rgrjsdb
 MongoClient.connect(process.env.MONGO_URL, (err, database) => {
   if (err) throw err;
   db = database;
