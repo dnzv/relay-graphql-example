257e191c7c3406a6ba04f22035ae45e98275474d
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-02-12 21:08:58 +0200

    Add top level store fragment to schema

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import API from '../API';
-import LinkStore from '../stores/LinkStore';
 
 class Main extends React.Component {
-
-  state = { links: LinkStore.getAll() };
-
-  componentWillMount() {
-    LinkStore.addChangeListener(this.onChange);
-  }
-
-  componentDidMount() {
-    API.fetchLinks();
-  }
-
-  componentWillUnmount() {
-    LinkStore.removeChangeListener(this.onChange);
-  }
-
   render() {
     const linkNodes = this.state.links.map(link =>
       <li key={link._id}><a href={link.url}>{link.title}</a></li>
@@ -32,10 +15,6 @@ class Main extends React.Component {
       </div>
     );
   }
-
-  onChange = () => {
-    this.setState({ links: LinkStore.getAll() });
-  };
 }
 
 export default Main;
