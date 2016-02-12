0bfb9757e18de3b92a0cfe4dcb345ee77dafe549
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-02-12 20:19:56 +0200

    Fix schema update

--- utils/updateSchema.js
+++ utils/updateSchema.js
@@ -1,23 +1,17 @@
 import fs from 'fs';
-import path from 'path';
-import { Schema } from '../data/schema';
+import Schema from '../data/schema';
+import { env } from './params';
+import {MongoClient} from 'mongodb';
 import { graphql }  from 'graphql';
 import { introspectionQuery, printSchema } from 'graphql/utilities';
 
-console.log('Creating schema.json...\n');
-
 (async () => {
-  let json = await graphql(Schema, introspectionQuery);
-  if (json.errors) {
-    console.error(
-      'ERROR introspecting schema: ',
-      JSON.stringify(json.errors, null, 2)
-    );
+  let db = await MongoClient.connect(env.mongoUrl);
+  let result = await graphql(Schema(db), introspectionQuery);
+  if (result.errors) {
+    console.error('ERROR introspecting schema: ', result.errors);
   } else {
-    fs.writeFileSync(
-      path.join(__dirname, '../data/schema.json'),
-      JSON.stringify(json, null, 2),
-      (err) => {
+    fs.writeFile('./data/schema.json', JSON.stringify(result, null, 2), (err) => {
         if (err) throw err;
         console.log("schema.json created.");
       }
