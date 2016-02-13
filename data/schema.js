cd0e2db46d506d085bfeedfe71913fd5a658e3ba
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-02-13 17:46:26 +0200

    Add createLinkMutation to the schema

--- data/schema.js
+++ data/schema.js
@@ -8,9 +8,11 @@ import {
   GraphQLID
 } from 'graphql';
 import {
+  globalIdField,
   connectionDefinitions,
   connectionArgs,
-  connectionFromPromisedArray
+  connectionFromPromisedArray,
+  mutationWithClientMutationId
 } from 'graphql-relay';
 
 let Schema = (db) => {
@@ -19,6 +21,7 @@ let Schema = (db) => {
   let storeType = new GraphQLObjectType({
     name: 'Store',
     fields: () => ({
+      id: globalIdField("Store"),
       linkConnection: {
         type: linkConnection.connectionType,
         args: connectionArgs,
@@ -48,6 +51,27 @@ let Schema = (db) => {
     nodeType: linkType
   });
 
+  let createLinkMutation = new mutationWithClientMutationId({
+    name: 'CreateLink',
+    inputFields: {
+      title: { type: new GraphQLNonNull(GraphQLString) },
+      url: { type: new GraphQLNonNull(GraphQLString) }
+    },
+    outputFields: {
+      linkEdge: {
+        type: linkConnection.edgeType,
+        resolve: (obj) => ({ node: obj.ops[0], cursor: obj.insertedId })
+      },
+      store: {
+        type: storeType,
+        resolve: () => store
+      }
+    },
+    mutateAndGetPayload: ({title, url}) => {
+      return db.collection("links").insertOne({title, url});
+    }
+  });
+
   let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
       name: 'Query',
@@ -56,6 +80,12 @@ let Schema = (db) => {
           type: storeType,
           resolve: () => store
         }
+      })
+    }),
+    mutation: new GraphQLObjectType({
+      name: 'Mutation',
+      fields: () => ({
+        createLink: createLinkMutation
       })
     })
   });
