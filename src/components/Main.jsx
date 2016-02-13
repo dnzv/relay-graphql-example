518d683e765ea18b4fdaaa88ffdae1cbc471d0d0
Deniz Vahaboglu <dnzv@users.noreply.github.com>
2016-02-13 20:21:00 +0200

    Modify Main component to use createLink mutation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Relay from 'react-relay';
 import Link from './Link';
+import CreateLinkMutation from '../mutations/CreateLinkMutation';
 
 class Main extends React.Component {
   render() {
@@ -11,10 +12,17 @@ class Main extends React.Component {
     return (
       <div>
         <h3>Links</h3>
-        <select onChange={this.setLimit}>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" ref="newTitle"/>
+          <input type="text" placeholder="Url" ref="newUrl"/>
+          <button type="submit">Add</button>
+        </form>
+        <select onChange={this.setLimit}
+                defaultValue={this.props.relay.variables.limit}>
           <option value="3">3</option>
           <option value="5">5</option>
-          <option value="10" selected>10</option>
+          <option value="10">10</option>
+          <option value="100">100</option>
         </select>
         <ul>
           {linkNodes}
@@ -27,15 +35,29 @@ class Main extends React.Component {
     let newLimit = Number(e.target.value);
     this.props.relay.setVariables({limit: newLimit});
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    Relay.Store.commitUpdate(
+      new CreateLinkMutation({
+        title: this.refs.newTitle.value,
+        url: this.refs.newUrl.value,
+        store: this.props.store
+      })
+    );
+    this.refs.newTitle.value = "";
+    this.refs.newUrl.value = "";
+  };
 }
 
 Main = Relay.createContainer(Main, {
   initialVariables: {
-    limit: 10
+    limit: 100
   },
   fragments: {
     store: () => Relay.QL `
       fragment on Store {
+        id,
         linkConnection(first: $limit) {
           edges {
             node {
